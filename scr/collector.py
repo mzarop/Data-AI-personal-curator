1b112f5e2bc55f1e0ea02a9c61f5e896ffe65c75
mzari <zaropoulosmix@gmail.com>
2025-10-19 12:04:16 +0300

    set app the environment

--- scr/collector.py
+++ scr/collector.py
@@ -1,4 +1,5 @@
 import feedparser
+import csv
 
 # RSS feed URL
 rss_url = "https://news.google.com/rss"
@@ -8,3 +9,27 @@ feed = feedparser.parse(rss_url)
 
 # Print feed title
 print(f"Feed Title: {feed.feed.title}\n")
+
+for entry in feed.entries[:5]:
+    print(f" Title: {entry.title}")
+    print(f" Link:{entry.link}")
+    print(f" Published: {entry.published}")
+    print(f" Summary: {entry.summary[:200]}...") # first 100 characters
+    print(" -"*40)
+
+# create an empty dictionary to insert the data
+dict_data = {"Title":[],
+             "Link":[],
+             "Published":[]}
+
+for entry in feed.entries[:5]:
+    dict_data["Title"].append(entry.title)
+    dict_data['Link'].append(entry.link)
+    dict_data['Published'].append(entry.published)
+
+# save data as csv under data folder
+with open("../data/output_data.csv","w",newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(dict_data.keys())
+    writer.writerows(zip(*dict_data.values()))
+
